e81353d9a0b067adc237ec66cab7ff2478088d18
Shikubro90 <shikubro90@gmail.com>
2023-01-27 17:40:10 +0600

    first commit

---
+++ sarver.js
@@ -0,0 +1,23 @@
+const {readdirSync} = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const morgan = require("morgan")
+const cors = require("cors")
+
+
+// middleware
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(helmet())
+
+// routes middleware
+readdirSync("./routes").map((r)=>app.use("/api/v1"),require(`./routes/${r}`))
+
+// server
+const port = process.env.PORT || 8000;
