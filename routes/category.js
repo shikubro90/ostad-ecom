d20da94decbdbb3b33e4fa9d832f5d14420a8f5e
shikubro90 <shikubro90@gmail.com>
2023-02-15 04:55:24 +0600

    update

--- routes/category.js
+++ routes/category.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { requireSignIn, isAdmin } = require("../middlewares/auth");
 const router = express.Router();
 
-<<<<<<< HEAD
 const { createCategory, update, remove , list, read} = require("../controllers/category");
 
 router.post("/create-category", requireSignIn, isAdmin, createCategory);
@@ -10,12 +9,5 @@ router.put("/update-category/:categoryId", requireSignIn, isAdmin, update);
 router.delete("/remove/:categoryId",requireSignIn, isAdmin, remove)
 router.get("/list",requireSignIn, isAdmin, list)
 router.get("/category/:slug", read)
-=======
-const { createCategory, update, getCollections } = require("../controllers/category");
-
-router.post("/create-category", requireSignIn, isAdmin, createCategory);
-router.put("/update-category/:categoryId", requireSignIn, isAdmin, update);
-router.get("/get-collections",requireSignIn, isAdmin, getCollections)
->>>>>>> e0adad6e8efb7c45780d68ad20f4b1f8d907dacb
 
 module.exports = router;
